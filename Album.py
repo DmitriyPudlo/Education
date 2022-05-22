e1594c6290a2499838cbeae78bc35d41df4869be
DmitriyPudlo <teinalandergor@gmail.com>
2022-05-22 15:38:31 +0500

    with __str__ for track and album

--- Album.py
+++ Album.py
@@ -3,8 +3,8 @@ class Track:
         self.name = str(name)
         self.time = int(time)
 
-    def show(self):
-        return f'{self.name} - {self.time}'
+    def __str__(self):
+        return f'{self.name} - {self.time} min'
 
 
 class Album:
@@ -14,8 +14,7 @@ class Album:
         self.list_song = []
 
     def get_tracks(self):
-        for song in self.list_song:
-            print(song.show())
+        return '\n'.join([song.__str__() for song in self.list_song])
 
     def add_track(self, track):
         if isinstance(track, Track):
@@ -25,16 +24,30 @@ class Album:
 
     def get_duration(self):
         list_time = [song.time for song in self.list_song]
-        return sum(list_time)
+        return f'{sum(list_time)} min'
+
+    def __str__(self):
+        return f'''
+Name group: {self.group}
+Name album: '{self.name}'
+Tracks:
+{self.get_tracks()}
+'''
 
 
-steal_is_album = Album('Steal This Album!', 'System of a Down')
+steal_is_album = Album('Steal This Album!', 'System of A Down')
 boom = Track('Boom!', 5)
 add = Track('A.D.D.', 3)
 mr_jack = Track('Mr.Jack', 4)
 
 steal_is_album.add_track(boom)
+steal_is_album.add_track(add)
 steal_is_album.add_track(mr_jack)
 
-steal_is_album.get_tracks()
+print(steal_is_album.get_tracks())
+
 print(steal_is_album.get_duration())
+
+print(boom)
+
+print(steal_is_album)
