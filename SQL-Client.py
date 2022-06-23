31a114aca6580d04c92b479ac61ce883ee3b4526
DmitriyPudlo <teinalandergor@gmail.com>
2022-06-24 00:09:34 +0500

    0.95

--- SQL-Client.py
+++ SQL-Client.py
@@ -78,16 +78,98 @@ def add_phone(conn, client_id, phone):
             WHERE number = '{phone}');'''
             )
         if not cursor.fetchone()[0]:
-            cursor.execute(
-                f'''INSERT INTO number_phone (number, id_client)
-                VALUES ('{phone}', {client_id});'''
-                )
+            cursor.execute(f'''INSERT INTO number_phone (number, id_client) VALUES ('{phone}', {client_id});''')
             print(f'Номер телефона клиенту {client_id} добавлен!')
         else:
             print(f'Номер телефона у клиента {client_id} уже существует!')
 
 
+def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+    with conn.cursor() as cursor:
+        if first_name is not None:
+            cursor.execute(f"UPDATE client SET first_name = '{first_name}' WHERE id = {client_id}")
+        if last_name is not None:
+            cursor.execute(f"UPDATE client SET last_name = '{last_name}' WHERE id = {client_id}")
+        if email is not None:
+            cursor.execute(f"UPDATE client SET e_mail = '{email}' WHERE id = {client_id}")
+        if phones is not None:
+            cursor.execute(f'DELETE FROM number_phone WHERE id_client = {client_id};')
+            for phone in phones.split():
+                cursor.execute(f'''INSERT INTO number_phone (number, id_client) VALUES ('{phone}', {client_id});''')
+        print(f'Данные о клиенте {client_id} обновлены!')
+
+
+def delete_phone(conn, client_id, phone):
+    with conn.cursor() as cursor:
+        cursor.execute(f"DELETE FROM number_phone WHERE id_client = {client_id} and number = '{phone}'")
+        print(f'Номер телефона у клиента {client_id} удален!')
+
+
+def delete_client(conn, client_id):
+    with conn.cursor() as cursor:
+        cursor.execute(
+            f'''SELECT EXISTS(
+            SELECT 1 AS result
+            FROM number_phone
+            WHERE id_client = '{client_id}');'''
+            )
+        if cursor.fetchone()[0]:
+            cursor.execute(f"DELETE FROM number_phone WHERE id_client = {client_id}")
+        cursor.execute(f"DELETE FROM client WHERE id = {client_id}")
+        print(f'Информация о клиенте {client_id} удалена!')
+
+
+def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+    with conn.cursor() as cursor:
+        if email is not None:
+            cursor.execute(f"SELECT * FROM client WHERE e_mail = '{email}'")
+            print(cursor.fetchall())
+            cursor.execute(
+                f'''SELECT number FROM number_phone WHERE id_client = (SELECT id FROM client WHERE e_mail = '{email}')''')
+            print(cursor.fetchall())
+            return
+        if phone is not None:
+            cursor.execute(
+                f'''SELECT * FROM client 
+                WHERE id = (SELECT id_client FROM number_phone WHERE number = '{phone}')''')
+            print(cursor.fetchall())
+            cursor.execute(
+                f'''SELECT number FROM number_phone 
+                WHERE id_client = (SELECT id_client FROM number_phone WHERE number = '{phone}')''')
+            print(cursor.fetchall())
+            return
+        if first_name is not None and last_name is None:
+            cursor.execute(f"SELECT * FROM client WHERE first_name = '{first_name}'")
+            print(cursor.fetchall())
+            cursor.execute(
+                f'''SELECT number FROM number_phone 
+                WHERE id_client IN (SELECT id FROM client WHERE first_name = '{first_name}')''')
+            print(cursor.fetchall())
+            return
+        if last_name is not None and first_name is None:
+            cursor.execute(f"SELECT * FROM client WHERE last_name = '{last_name}'")
+            print(cursor.fetchall())
+            cursor.execute(
+                f'''SELECT number FROM number_phone 
+                WHERE id_client IN (SELECT id FROM client WHERE last_name = '{last_name}')''')
+            print(cursor.fetchall())
+            return
+        if last_name is not None and first_name is not None:
+            cursor.execute(f"SELECT * FROM client WHERE last_name = '{last_name}' and first_name = '{first_name}'")
+            print(cursor.fetchall())
+            cursor.execute(
+                f'''SELECT number FROM number_phone 
+                WHERE id_client IN (SELECT id FROM client 
+                WHERE last_name = '{last_name}' and first_name = '{first_name}')''')
+            print(cursor.fetchall())
+            return
+
 with psycopg2.connect(dbname='postgres', user='postgres', password='3616', host='localhost') as conn:
     create_db(conn)
     add_client(conn, 'Dima', 'PUDLO', 'dpmail.ru', phones='89228513682 89292825456')
+    add_client(conn, 'Sergey', 'PUDLO', 'spmail.ru', phones='123456789')
     add_phone(conn, 1, '89228513682')
+    change_client(conn, 1, phones='123123123123 321321321321')
+    delete_phone(conn, 1, phone='123123123123')
+    delete_client(conn, 2)
+    find_client(conn, first_name=None, last_name=None, email=None, phone=None)
