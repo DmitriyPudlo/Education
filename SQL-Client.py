8ed1781815a5f5d1a54ba110935a679140c5e977
DmitriyPudlo <teinalandergor@gmail.com>
2022-06-25 22:59:30 +0500

    0,9

--- SQL-Client.py
+++ SQL-Client.py
@@ -19,7 +19,7 @@ def add_client(conn, first_name, last_name, email, phones=None):
                 f'''INSERT INTO number_phone (number, id_client)
                 VALUES ('{phone}', (SELECT id 
                 FROM client 
-                WHERE e_mail = '{email}');'''
+                WHERE e_mail = '{email}'));'''
         print('Клиент добавлен!')
 
 
@@ -36,7 +36,8 @@ def change_client(conn, client_id, first_name=None, last_name=None, e_mail=None,
             if value is not None and key == 'phones':
                 cursor.execute(f'DELETE FROM number_phone WHERE id_client = {client_id};')
                 for phone in phones.split():
-                    cursor.execute(f'''INSERT INTO number_phone (number, id_client) VALUES ('{phone}', {client_id});''')
+                    cursor.execute(f'''INSERT INTO number_phone (number, id_client) 
+                                    VALUES ('{phone}', {client_id});''')
             elif value is not None:
                 cursor.execute(f"UPDATE client SET {key} = '{value}' WHERE id = {client_id}")
     print(f'Данные о клиенте {client_id} обновлены!')
@@ -57,65 +58,26 @@ def delete_client(conn, client_id):
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     with conn.cursor() as cursor:
-        if email is not None:
-            cursor.execute(f"SELECT * FROM client WHERE e_mail = '{email}'")
-            print(cursor.fetchall())
-            cursor.execute(
-                f'''SELECT number 
-                FROM number_phone 
-                WHERE id_client = (SELECT id 
-                FROM client 
-                WHERE e_mail = '{email}')''')
-            print(cursor.fetchall())
-            return
-        if phone is not None:
-            cursor.execute(
-                f'''SELECT * 
-                FROM client 
-                WHERE id = (SELECT id_client 
-                FROM number_phone 
-                WHERE number = '{phone}')''')
-            print(cursor.fetchall())
-            cursor.execute(
-                f'''SELECT number 
-                FROM number_phone 
-                WHERE id_client = (SELECT id_client 
-                FROM number_phone 
-                WHERE number = '{phone}')''')
-            print(cursor.fetchall())
-            return
-        if first_name is not None and last_name is None:
-            cursor.execute(f"SELECT * FROM client WHERE first_name = '{first_name}'")
-            print(cursor.fetchall())
-            cursor.execute(
-                f'''SELECT number FROM number_phone 
-                WHERE id_client IN (SELECT id FROM client WHERE first_name = '{first_name}')''')
-            print(cursor.fetchall())
-            return
-        if last_name is not None and first_name is None:
-            cursor.execute(f"SELECT * FROM client WHERE last_name = '{last_name}'")
-            print(cursor.fetchall())
-            cursor.execute(
-                f'''SELECT number FROM number_phone 
-                WHERE id_client IN (SELECT id FROM client WHERE last_name = '{last_name}')''')
-            print(cursor.fetchall())
-            return
-        if last_name is not None and first_name is not None:
-            cursor.execute(f"SELECT * FROM client WHERE last_name = '{last_name}' and first_name = '{first_name}'")
-            print(cursor.fetchall())
-            cursor.execute(
-                f'''SELECT number FROM number_phone 
-                WHERE id_client IN (SELECT id FROM client 
-                WHERE last_name = '{last_name}' and first_name = '{first_name}')''')
-            print(cursor.fetchall())
-            return
+        values_for_select = {'first_name': first_name, 'last_name': last_name, 'e_mail': email, 'phone': phone}
+        for key, value in values_for_select.items():
+            if value is not None:
+                cursor.execute(f'''
+                SELECT first_name, last_name, e_mail
+                FROM client
+                JOIN number_phone
+                ON client.id = number_phone.id_client
+                WHERE {key} = '{value}'
+                ''')
+                client_info = cursor.fetchall()[0]
+                print(f'Имя: {client_info[0]}, Фамилия: {client_info[1]}, e-mail: {client_info[2]}')
+                break
 
 with psycopg2.connect(dbname='postgres', user='postgres', password='3616', host='localhost') as conn:
     create_db(conn)
     add_client(conn, 'Dima', 'PUDLO', 'dpmail.ru', phones='89228513682 89292825456')
-    # add_client(conn, 'Sergey', 'PUDLO', 'spmail.ru', phones='123456789')
-    # add_phone(conn, 1, '89228513682')
-    # change_client(conn, 1, phones='123123123123 321321321321')
-    # delete_phone(conn, 1, phone='123123123123')
-    # delete_client(conn, 2)
-    # find_client(conn, first_name=None, last_name=None, email=None, phone=None)
+    add_client(conn, 'Sergey', 'PUDLO', 'spmail.ru', phones='123456789')
+    add_phone(conn, 1, '89228513682')
+    change_client(conn, 1, phones='123123123123 321321321321')
+    delete_phone(conn, 1, phone='123123123123')
+    delete_client(conn, 2)
+    find_client(conn, first_name='Dima')
