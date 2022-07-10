b59de7fe8ba4fbd20a4afc3cc3471a4a50605bf2
DmitriyPudlo <teinalandergor@gmail.com>
2022-07-10 13:36:49 +0500

    complete

--- decorators.py
+++ decorators.py
@@ -1,7 +1,19 @@
 from datetime import datetime
 
 
-def Create_log(func):
+def add_path(path):
+    def create_log_on_place(func):
+        def wrapper(*args, **kwargs):
+            with open(f'{path}{func.__name__}_log.txt', 'w') as log:
+                res = func(*args, **kwargs)
+                info = [f'{datetime.today()}, {func.__name__}, {args}, {kwargs}, {res}']
+                log.writelines(info)
+            return res
+        return wrapper
+    return create_log_on_place
+
+
+def create_log(func):
     def wrapper(*args, **kwargs):
         with open(f'{func.__name__}_log.txt', 'w') as log:
             res = func(*args, **kwargs)
@@ -11,7 +23,7 @@ def Create_log(func):
     return wrapper
 
 
-@Create_log
+@add_path('D:/')
 def hello(x):
     return f'Hello {x}'
 
