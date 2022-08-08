b08ab302e8703645548265a259fb6413f22e1f0d
DmitriyPudlo <teinalandergor@gmail.com>
2022-08-08 23:09:58 +0500

    complete

--- scrupper.py
+++ scrupper.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://habr.com/ru/all/'
+url = 'https://2ip.ru/'
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, features='lxml')
 
-soup = soup.find('div', class_='tm-article-snippet')
+soup = soup.find('div', class_='ip-info').find('div', class_='ip').find('span').text
 
 print(soup)
