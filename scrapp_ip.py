bd722a3e0db5a855941e0e32c6effbb980f68983
DmitriyPudlo <teinalandergor@gmail.com>
2022-08-08 23:22:03 +0500

    complete

--- scrapp_ip.py
+++ scrapp_ip.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://habr.com/ru/all/'
+url = 'https://2ip.ru/'
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, features='lxml')
 
-soup = soup.find('a', class_='tm-article-snippet__hubs-item-link')
+soup = soup.find('div', class_='ip').find('span').text
 
 print(soup)
